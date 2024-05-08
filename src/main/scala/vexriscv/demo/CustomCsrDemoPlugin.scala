770996a91738268b0f9d00d3c3bc94222d143a90
elialaz <elia.lazzeri@mail.polimi.it>
2024-05-08 13:17:29 +0200

    crs custom 4

--- src/main/scala/vexriscv/demo/CustomCsrDemoPlugin.scala
+++ src/main/scala/vexriscv/demo/CustomCsrDemoPlugin.scala
@@ -35,7 +35,7 @@ class CustomCsrDemoPlugin extends Plugin[VexRiscv]{
       csrService.r(0xB05, cycleCounter)
       csrService.r(CustomCsr.STCYCLE, stcycle)
       csrService.onWrite(CustomCsr.NEW_INSTRUCTION){
-        stcycle := 0 // Set stcycle to 0 when the new instruction is executed
+        stcycle := 0 // Set stcycle to 0 when the new instruction is execute
       }
       csrService.onWrite(0xB06){
         instructionCounter := 0
